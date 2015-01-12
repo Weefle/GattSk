63e88c8f422688e9b62f5d9ac4768735092da62d
Funnygatt <zacharygatt@me.com>
2015-01-12 22:27:26 +1100

    Added some NBT Stuff
    Yuppy

--- src/me/Funnygatt/GattSK/Effects/General/misc.java
+++ src/me/Funnygatt/GattSK/Effects/General/misc.java
@@ -1,10 +1,14 @@
 package me.Funnygatt.GattSK.Effects.General;
 
+import net.minecraft.server.v1_8_R1.EntityInsentient;
+import net.minecraft.server.v1_8_R1.GenericAttributes;
 import net.skquery.api.annotation.LookThrough;
 import net.skquery.api.annotation.ToSkript;
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftLivingEntity;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
@@ -20,6 +24,37 @@ public class misc {
 //		}
 //	}
 	//TODO: Convert to SkQueryAPI once w00t tells me how to do this >.>
+
+	@ToSkript("set knockback resistance of %entities% to %integer%")
+	public static void setKnockbackResistance(Entity[] entities, Integer integer){
+		for (Entity e : entities){
+			EntityInsentient nmsEntity = (EntityInsentient) ((CraftLivingEntity) e).getHandle();
+			nmsEntity.getAttributeInstance(GenericAttributes.c).setValue(integer.doubleValue());
+		}
+	}
+	@ToSkript("set attack damage of %entities% to %integer%")
+	public static void setAttackDamage(Entity[] entities, Integer integer){
+		for (Entity e : entities){
+			EntityInsentient nmsEntity = (EntityInsentient) ((CraftLivingEntity) e).getHandle();
+			nmsEntity.getAttributeInstance(GenericAttributes.e).setValue(integer.doubleValue());
+		}
+	}
+
+	@ToSkript("set movement speed of %entities% to %integer%")
+	public static void setMovementSpeed(Entity[] entities, Integer integer){
+		for (Entity e : entities){
+			EntityInsentient nmsEntity = (EntityInsentient) ((CraftLivingEntity) e).getHandle();
+			nmsEntity.getAttributeInstance(GenericAttributes.d).setValue(integer.doubleValue());
+		}
+	}
+
+	@ToSkript("set follow range of %entities% to %integer%")
+	public static void setFollowRange(Entity[] entities, Integer integer){
+		for (Entity e : entities){
+			EntityInsentient nmsEntity = (EntityInsentient) ((CraftLivingEntity) e).getHandle();
+			nmsEntity.getAttributeInstance(GenericAttributes.b).setValue(integer.doubleValue());
+		}
+	}
 
 	@ToSkript("update inventory of %player%")
 	public static void updateInventory(Player[] players){
